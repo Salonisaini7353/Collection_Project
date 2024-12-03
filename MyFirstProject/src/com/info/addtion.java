1d1f4851379ce19f41c7df252921b70feffd3e73
dell <dell@DESKTOP-QJ0HOL5>
2024-12-03 16:40:54 +0530

    no change

---
+++ MyFirstProject/src/com/info/addtion.java
@@ -0,0 +1,13 @@
+package com.info;
+
+public class addtion {
+	public static void main (string arg[]) {
+  int x = 2000;
+  int y = 1000;
+  int z = a+b;
+system.out.print("addtion is " z );
+
+	
+
+}
+}
